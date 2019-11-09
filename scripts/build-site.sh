a3d96fb37d33b97b91aaba15c9bbebfc870ef473
Andi Huber <ahuber@apache.org>
2019-11-09 21:43:41 +0100

    fix typo

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -11,5 +11,3 @@ $(npm bin)/antora --stacktrace $SITE_CONFIG
 
 # add a marker, tells github not to use jekyll on the github pages folder
 touch ${PROJECT_ROOT_PATH}/antora/target/site/.nojekyll
-
-
