5b4a11d98771f8226cf0af9ab511e6d57ce197f9
Andi Huber <ahuber@apache.org>
2019-11-10 11:07:32 +0100

    removes source dir renaming workaround, not needed

--- scripts/rsync-site.sh
+++ scripts/rsync-site.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-# rename the rsync source because rsync's delete option does not work with wildcards eg /antora/target/site/*  
-mv $PROJECT_ROOT_PATH/antora/target/site $PROJECT_ROOT_PATH/antora/target/docs
-
 # rsync verbose, recursive and skip based on checksum
-rsync -avc --delete --exclude ".git" $PROJECT_ROOT_PATH/antora/target/docs/ $NIGHTLY_ROOT_PATH/docs
+# note: rsync's delete option does not work with wildcards eg antora/target/site/*
+rsync -avc --delete --exclude ".git" $PROJECT_ROOT_PATH/antora/target/site/ $NIGHTLY_ROOT_PATH/docs
