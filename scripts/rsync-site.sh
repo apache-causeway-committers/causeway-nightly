247a037dae4c7fd3e8dc9f2099450a3372cf8026
Andi Huber <ahuber@apache.org>
2019-11-10 11:02:53 +0100

    fixes rsync not deleting at target dir (2)

--- scripts/rsync-site.sh
+++ scripts/rsync-site.sh
@@ -5,4 +5,4 @@ set -e
 mv $PROJECT_ROOT_PATH/antora/target/site $PROJECT_ROOT_PATH/antora/target/docs
 
 # rsync verbose, recursive and skip based on checksum
-rsync -avc --delete --exclude ".git" $PROJECT_ROOT_PATH/antora/target/docs $NIGHTLY_ROOT_PATH/docs
+rsync -avc --delete --exclude ".git" $PROJECT_ROOT_PATH/antora/target/docs/ $NIGHTLY_ROOT_PATH/docs
