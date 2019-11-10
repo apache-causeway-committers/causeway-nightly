951ba7ff9ca98c711f474c71a6c90f0d1c478728
Andi Huber <ahuber@apache.org>
2019-11-10 10:27:06 +0100

    remove rsync stage debug file

--- scripts/rsync-site.sh
+++ scripts/rsync-site.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-touch $PROJECT_ROOT_PATH/antora/target/site/dummy.txt
+#debug
+#touch $PROJECT_ROOT_PATH/antora/target/site/dummy.txt
 
 # rsync verbose, recursive and skip based on checksum
 rsync -avc --delete --exclude ".git" $PROJECT_ROOT_PATH/antora/target/site/* $NIGHTLY_ROOT_PATH/docs
-
-ls -al $PROJECT_ROOT_PATH/antora/target/site
