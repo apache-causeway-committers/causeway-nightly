ce4b58273979fa88a6bfe289c4684342a0f4809f
Andi Huber <ahuber@apache.org>
2019-11-12 18:18:35 +0100

    for REVISION need to use SHA of 'isis' not 'isis-nightly' (6)

--- scripts/nightly-new-revision.sh
+++ scripts/nightly-new-revision.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
-MASTER_SHA8 = `curl -s --request GET \
+MASTER_SHA8=`curl -s --request GET \
         --url https://api.github.com/repos/apache/isis/git/ref/heads/master \
         --header 'content-type: application/json' \
         | grep sha | cut -d\: -f2 | cut -d\" -f2 | cut -c1-8`
 
-LATEST_REV = `curl -s GET https://repo.incode.cloud/repository/maven/org/apache/isis/core/isis/maven-metadata.xml | xmllint --xpath 'string(//metadata/versioning/latest)'`
-LATEST_SHA8 = ${LATEST_REV: -8}  
+LATEST_REV=`curl -s GET \
+        https://repo.incode.work/org/apache/isis/core/isis/maven-metadata.xml \
+        | grep release | cut -d\> -f2 | cut -d\< -f1`
+LATEST_SHA8=${LATEST_REV: -8}  
 
+echo "MASTER_SHA8: ${MASTER_SHA8}"
+echo "LATEST_REV: ${LATEST_REV}"
+echo "LATEST_SHA8: ${LATEST_SHA8}"
 
-if [ "$x" = "valid" ]; then
+if [ "$MASTER_SHA8" = "$LATEST_SHA8" ]; then
   echo "skipping update, are same MASTER_SHA8: ${MASTER_SHA8}, LATEST_SHA8: ${LATEST_SHA8}"
 else
   echo "##[set-output name=revision;]${BASELINE}.$(date +%Y%m%d-%H%M)-${MASTER_SHA8}"
