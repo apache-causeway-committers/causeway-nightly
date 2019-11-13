7a0c30b44f6458d9ac71cc7bec2b1cfc776f1d51
Andi Huber <ahuber@apache.org>
2019-11-13 10:47:18 +0100

    for REVISION need to use SHA of 'isis' not 'isis-nightly' (8)

--- scripts/nightly-new-revision.sh
+++ scripts/nightly-new-revision.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-# isis master commit hash
+## isis master commit hash
 MASTER_SHA=`curl -s --request GET \
         --url https://api.github.com/repos/apache/isis/git/ref/heads/master \
         --header 'content-type: application/json' \
         | grep sha | cut -d\: -f2 | cut -d\" -f2`
 
-# latest published commit hash: grepping the maven-metadata.xml this way is fragile
+## latest published commit hash: grepping the maven-metadata.xml this way is fragile
 PUBLISHED_SHA=`curl -s GET \
         https://repo.incode.work/org/apache/isis/core/isis/maven-metadata.xml \
         | grep release | cut -d\> -f2 | cut -d\< -f1`
@@ -16,11 +16,14 @@ PUBLISHED_SHA=`curl -s GET \
 MASTER_SHA8=${MASTER_SHA: -8}        
 PUBLISHED_SHA8=${PUBLISHED_SHA: -8}  
 
-#echo "MASTER_SHA8: ${MASTER_SHA8}"
-#echo "LATEST_SHA8: ${LATEST_SHA8}"
+## debug
+echo "MASTER_SHA:    ${MASTER_SHA}"
+echo "PUBLISHED_SHA: ${PUBLISHED_SHA}"
 
 if [ "$MASTER_SHA8" = "$PUBLISHED_SHA8" ]; then
-  echo "skipping update, are same MASTER_SHA8: ${MASTER_SHA8}, PUBLISHED_SHA8: ${PUBLISHED_SHA8}"
+  echo "skipping update, because (shortened) hashes are equal:"
+  echo "- MASTER_SHA8:    ${MASTER_SHA8}"
+  echo "- PUBLISHED_SHA8: ${PUBLISHED_SHA8}"
 else
   echo "##[set-output name=revision;]${BASELINE}.$(date +%Y%m%d-%H%M)-${MASTER_SHA8}"
 fi
