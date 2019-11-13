9ee8656f499f0f1012e5d24ee6c3179f79fb4180
Andi Huber <ahuber@apache.org>
2019-11-13 10:27:41 +0100

    for REVISION need to use SHA of 'isis' not 'isis-nightly' (7)

--- scripts/nightly-new-revision.sh
+++ scripts/nightly-new-revision.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 set -e
 
-MASTER_SHA8=`curl -s --request GET \
+# isis master commit hash
+MASTER_SHA=`curl -s --request GET \
         --url https://api.github.com/repos/apache/isis/git/ref/heads/master \
         --header 'content-type: application/json' \
-        | grep sha | cut -d\: -f2 | cut -d\" -f2 | cut -c1-8`
+        | grep sha | cut -d\: -f2 | cut -d\" -f2`
 
-LATEST_REV=`curl -s GET \
+# latest published commit hash: grepping the maven-metadata.xml this way is fragile
+PUBLISHED_SHA=`curl -s GET \
         https://repo.incode.work/org/apache/isis/core/isis/maven-metadata.xml \
         | grep release | cut -d\> -f2 | cut -d\< -f1`
-LATEST_SHA8=${LATEST_REV: -8}  
+   
 
-echo "MASTER_SHA8: ${MASTER_SHA8}"
-echo "LATEST_REV: ${LATEST_REV}"
-echo "LATEST_SHA8: ${LATEST_SHA8}"
+MASTER_SHA8=${MASTER_SHA: -8}        
+PUBLISHED_SHA8=${PUBLISHED_SHA: -8}  
 
-if [ "$MASTER_SHA8" = "$LATEST_SHA8" ]; then
-  echo "skipping update, are same MASTER_SHA8: ${MASTER_SHA8}, LATEST_SHA8: ${LATEST_SHA8}"
+#echo "MASTER_SHA8: ${MASTER_SHA8}"
+#echo "LATEST_SHA8: ${LATEST_SHA8}"
+
+if [ "$MASTER_SHA8" = "$PUBLISHED_SHA8" ]; then
+  echo "skipping update, are same MASTER_SHA8: ${MASTER_SHA8}, PUBLISHED_SHA8: ${PUBLISHED_SHA8}"
 else
   echo "##[set-output name=revision;]${BASELINE}.$(date +%Y%m%d-%H%M)-${MASTER_SHA8}"
 fi
