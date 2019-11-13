8bdf73d630ea32246eda164d7645736008c585a1
Andi Huber <ahuber@apache.org>
2019-11-13 10:51:32 +0100

    for REVISION need to use SHA of 'isis' not 'isis-nightly' (9)

--- scripts/nightly-new-revision.sh
+++ scripts/nightly-new-revision.sh
@@ -7,18 +7,22 @@ MASTER_SHA=`curl -s --request GET \
         --header 'content-type: application/json' \
         | grep sha | cut -d\: -f2 | cut -d\" -f2`
 
-## latest published commit hash: grepping the maven-metadata.xml this way is fragile
-PUBLISHED_SHA=`curl -s GET \
+## latest published revision: eg. 2.0.0-M2.20191113-0401-ce4b5827 
+## grepping the maven-metadata.xml this way is fragile
+PUBLISHED_REV=`curl -s GET \
         https://repo.incode.work/org/apache/isis/core/isis/maven-metadata.xml \
         | grep release | cut -d\> -f2 | cut -d\< -f1`
    
 
 MASTER_SHA8=${MASTER_SHA: -8}        
-PUBLISHED_SHA8=${PUBLISHED_SHA: -8}  
+PUBLISHED_SHA8=${PUBLISHED_REV: -8}  
 
 ## debug
-echo "MASTER_SHA:    ${MASTER_SHA}"
-echo "PUBLISHED_SHA: ${PUBLISHED_SHA}"
+echo "MASTER_SHA:        ${MASTER_SHA}"
+echo "PUBLISHED_REV:     ${PUBLISHED_REV}"
+echo "-> MASTER_SHA8:    ${MASTER_SHA8}"
+echo "-> PUBLISHED_SHA8: ${PUBLISHED_SHA8}"
+echo "##[set-output name=revision;]${BASELINE}.$(date +%Y%m%d-%H%M)-${MASTER_SHA8}"
 
 if [ "$MASTER_SHA8" = "$PUBLISHED_SHA8" ]; then
   echo "skipping update, because (shortened) hashes are equal:"
