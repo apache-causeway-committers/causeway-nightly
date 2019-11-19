44ca685bd4585469a291b90a9b6afd5f33f39c9a
danhaywood <dan@haywood-associates.co.uk>
2019-11-19 23:24:48 +0000

    uses scripts/ci from isis repo, rather than copies in isis-nightly.

--- scripts/nightly/build-core.sh
+++ scripts/nightly/build-core.sh
@@ -6,27 +6,21 @@ echo "Shared Vars File: ${SHARED_VARS_FILE}"
 # import shared vars (non secret!)
 source $SHARED_VARS_FILE && export $(cut -d= -f1 $SHARED_VARS_FILE)
 
-sh $CI_SCRIPTS_PATH/print-environment.sh "nightly-build-mixins"
+sh $CI_SCRIPTS_PATH/print-environment.sh "nightly-build-core"
 
-cd $PROJECT_ROOT_PATH/mixins
 
-# can't use flatten pom, so have to edit directly instead...
+cd $PROJECT_ROOT_PATH/core-parent
+
 mvn versions:set -DnewVersion=$REVISION
 
 # setting 'github-deploy.repositoryUrl' activates the 'github-deploy' maven profile as defined in the pom.xml
-# the 'github-deploy' profile is mapped to the server setting as declared in '.m2/nightly-settings.xml'  
+# the 'github-deploy' profile is mapped to the server setting as declared in '.m2/nightly-settings.xml'
 mvn -s $NIGHTLY_ROOT_PATH/.m2/nightly-settings.xml \
     --batch-mode \
     $MVN_STAGES \
-    -Drevision=$REVISION \
-    -Dskip.mavenmixin-standard \
-    -Dskip.mavenmixin-surefire \
-    -Dskip.mavenmixin-datanucleus-enhance \
+    -Dskip.assemble-zip \
     $MVN_ADDITIONAL_OPTS
 
-# revert the edits from earlier ...
 mvn versions:revert
 
 cd $PROJECT_ROOT_PATH
-
-
--- scripts/nightly/build-mixins.sh
+++ scripts/nightly/build-mixins.sh
@@ -6,11 +6,11 @@ echo "Shared Vars File: ${SHARED_VARS_FILE}"
 # import shared vars (non secret!)
 source $SHARED_VARS_FILE && export $(cut -d= -f1 $SHARED_VARS_FILE)
 
-sh $CI_SCRIPTS_PATH/print-environment.sh "nightly-build-parent"
+sh $CI_SCRIPTS_PATH/print-environment.sh "nightly-build-mixins"
 
-cd $PROJECT_ROOT_PATH/core-parent
+cd $PROJECT_ROOT_PATH/mixins
 
-# can't use flatten pom, so have to edit directly instead...
+# temporarily set version
 mvn versions:set -DnewVersion=$REVISION
 
 # setting 'github-deploy.repositoryUrl' activates the 'github-deploy' maven profile as defined in the pom.xml
@@ -18,6 +18,9 @@ mvn versions:set -DnewVersion=$REVISION
 mvn -s $NIGHTLY_ROOT_PATH/.m2/nightly-settings.xml \
     --batch-mode \
     $MVN_STAGES \
+    -Dskip.mavenmixin-standard \
+    -Dskip.mavenmixin-surefire \
+    -Dskip.mavenmixin-datanucleus-enhance \
     $MVN_ADDITIONAL_OPTS
 
 # revert the edits from earlier ...
