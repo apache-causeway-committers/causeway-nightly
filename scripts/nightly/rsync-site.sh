2da72fc191e7ee1d80a68decac2b21c21d367fbc
danhaywood <dan@haywood-associates.co.uk>
2020-11-22 13:29:14 +0000

    fixes rsync of site

--- scripts/nightly/rsync-site.sh
+++ scripts/nightly/rsync-site.sh
@@ -3,4 +3,4 @@ set -e
 
 # rsync verbose, recursive and skip based on checksum
 # note: rsync's delete option does not work with wildcards eg antora/target/site/*
-rsync -avc --delete --exclude ".git" $FRAMEWORK_ROOT_PATH/antora/target/site/ $NIGHTLY_ROOT_PATH/docs
+rsync -avc --delete --exclude ".git" $PROJECT_ROOT_PATH/antora/target/site/ $NIGHTLY_ROOT_PATH/docs
