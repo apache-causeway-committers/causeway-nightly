df85d1e90ba9f273a636fdb5067c9155222f0c4c
danhaywood <dan@haywood-associates.co.uk>
2021-03-12 10:20:41 +0000

    parameterizing deploy

--- k8s/update.sh
+++ k8s/update.sh
@@ -4,37 +4,37 @@ set -e
 DIRNAME=$(dirname $0)
 APPLICATION=$1
 BRANCH=$2
+BRANCH_LOWER=$(echo ${BRANCH} |  tr '[:upper:]' '[:lower:]' )
 
-APP="isis-app-${APPLICATION}-${BRANCH}"
-IMAGE="apacheisis/${APPLICATION}:latest"
-#IMAGE="apacheisis/${APPLICATION}:${BRANCH}"
-HOST="${APPLICATION}.${BRANCH}.isis.incode.work"
-SECRET="isis-app-${APPLICATION}-tls-${BRANCH}"
+APPNAME="isis-app-${APPLICATION}-${BRANCH_LOWER}"
+IMAGE="apacheisis/${APPLICATION}:${BRANCH}"
+HOST="${APPLICATION}.${BRANCH_LOWER}.isis.incode.work"
+SECRET="isis-app-${APPLICATION}-tls-${BRANCH_LOWER}"
 
-echo "APP    : $APP"
-echo "IMAGE  : $IMAGE"
-echo "HOST   : $HOST"
-echo "SECRET : $SECRET"
+echo "APPNAME : $APPNAME"
+echo "IMAGE   : $IMAGE"
+echo "HOST    : $HOST"
+echo "SECRET  : $SECRET"
 
 cd $DIRNAME/$APPLICATION
 
 echo "---"
-yq eval -i ".metadata.name = \"$APP\"" 010-deployment.yaml -i
-yq eval ".spec.selector.matchLabels.app = \"$APP\"" 010-deployment.yaml -M -i
-yq eval ".spec.template.metadata.labels.app = \"$APP\"" 010-deployment.yaml -M -i
+yq eval -i ".metadata.name = \"$APPNAME\"" 010-deployment.yaml -i
+yq eval ".spec.selector.matchLabels.app = \"$APPNAME\"" 010-deployment.yaml -M -i
+yq eval ".spec.template.metadata.labels.app = \"$APPNAME\"" 010-deployment.yaml -M -i
 yq eval ".spec.template.spec.containers[0].image = \"$IMAGE\"" 010-deployment.yaml -M -i
-yq eval ".spec.template.spec.containers[0].name = \"$APP\"" 010-deployment.yaml -M -i
+yq eval ".spec.template.spec.containers[0].name = \"$APPNAME\"" 010-deployment.yaml -M -i
 cat 010-deployment.yaml
 
 echo "---"
-yq eval ".metadata.name = \"$APP\"" 020-service.yaml -M -i
-yq eval ".spec.selector.app = \"$APP\"" 020-service.yaml -M -i
+yq eval ".metadata.name = \"$APPNAME\"" 020-service.yaml -M -i
+yq eval ".spec.selector.app = \"$APPNAME\"" 020-service.yaml -M -i
 cat 020-service.yaml
 
 echo "---"
-yq eval ".metadata.name = \"$APP\"" 030-ingress.yaml -M -i
+yq eval ".metadata.name = \"$APPNAME\"" 030-ingress.yaml -M -i
 yq eval ".spec.tls[0].hosts[0] = \"$HOST\"" 030-ingress.yaml -M -i
 yq eval ".spec.tls[0].secretName = \"$SECRET\"" 030-ingress.yaml -M -i
-yq eval ".spec.rules[0].http.paths[0].backend.serviceName = \"$APP\"" 030-ingress.yaml -M -i
+yq eval ".spec.rules[0].http.paths[0].backend.serviceName = \"$APPNAME\"" 030-ingress.yaml -M -i
 yq eval ".spec.rules[0].host = \"$HOST\"" 030-ingress.yaml -M -i
 cat 030-ingress.yaml
