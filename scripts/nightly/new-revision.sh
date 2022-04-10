d579a94acb6fedc96218558432c9d71bc26890d8
Andi Huber <ahuber@apache.org>
2022-04-10 11:21:01 +0200

    resurrect nightly build (2)

--- scripts/nightly/new-revision.sh
+++ scripts/nightly/new-revision.sh
@@ -9,10 +9,11 @@ MASTER_SHA=`curl -s --request GET \
 
 ## latest published revision: eg. 2.0.0-M2.20191113-0401-ce4b5827
 ## grepping the maven-metadata.xml this way is fragile
-PUBLISHED_REV=`curl -s GET \
-        https://nexus.incode.work/repository/nightly-builds/org/apache/isis/isis-parent/maven-metadata.xml \
-        | grep release | cut -d\> -f2 | cut -d\< -f1`
-
+#PUBLISHED_REV=`curl -s GET \
+#        https://nexus.incode.work/repository/nightly-builds/org/apache/isis/isis-parent/maven-metadata.xml \
+#        | grep release | cut -d\> -f2 | cut -d\< -f1`
+        
+PUBLISHED_REV=2.0.0-M2.20191113-0401-ce4b5827
 
 MASTER_SHA8=${MASTER_SHA: 0:8}
 PUBLISHED_SHA8=${PUBLISHED_REV: -8}
