cd05f3b0c3903cd80a663bb96d13bf1536a9d54a
Andi Huber <ahuber@apache.org>
2022-04-11 08:35:50 +0200

    resurrect nightly build (12)

--- scripts/nightly/rsync-site.sh
+++ scripts/nightly/rsync-site.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-# copy merged mvn-snapshots folder from DEST to SOURCE, so rsync (below) can safely ignore it
-cp $NIGHTLY_ROOT_PATH/docs/mvn-snapshots $PROJECT_ROOT_PATH/antora/target/site/  
+# move (already merged) mvn-snapshots folder from DEST to TMP, so rsync (below) can safely ignore it
+mv $NIGHTLY_ROOT_PATH/docs/mvn-snapshots /tmp/  
 
 # rsync verbose, recursive and skip based on checksum
 # note: rsync's delete option does not work with wildcards eg antora/target/site/*
 rsync -avc --delete --exclude ".git" $PROJECT_ROOT_PATH/antora/target/site/ $NIGHTLY_ROOT_PATH/docs
+
+# revert above move
+mv /tmp/mvn-snapshots $NIGHTLY_ROOT_PATH/docs/
