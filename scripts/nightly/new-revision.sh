26c848bb764a5a17bf7678b931ee22fd453232e2
Andi Huber <ahuber@apache.org>
2025-10-13 18:25:59 +0200

    Update new-revision.sh

--- scripts/nightly/new-revision.sh
+++ scripts/nightly/new-revision.sh
@@ -23,7 +23,7 @@ HEAD_SHA=`curl -s --request GET \
 ## latest published revision: eg. 2.0.0-M2.20191113-0401-ce4b5827
 ## grepping the maven-metadata.xml this way is fragile
 PUBLISHED_REV=`curl -s GET \
-		https://raw.githubusercontent.com/apache-causeway-committers/causeway-nightly/master/mvn-snapshots/org/apache/causeway/causeway-bom/maven-metadata.xml \
+		https://raw.githubusercontent.com/apache-causeway-committers/causeway-nightly/main/mvn-snapshots/org/apache/causeway/causeway-bom/maven-metadata.xml \
         | grep version | grep ${BASELINE} | tail -1 | cut -d\> -f2 | cut -d\< -f1`
 
 #debug
